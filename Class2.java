a1833d11512c4c9b23ca386ea5dbcab295959c8b
Yallapu <sai-ganesh.yallapu@capgemini.com>
2024-04-15 12:16:04 +0530

    first commit

---
+++ Class2.java
@@ -0,0 +1,12 @@
+package com.java.mypackage2;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld2");
+		System.out.println("Before changes");
+		
+	}
+
+}
